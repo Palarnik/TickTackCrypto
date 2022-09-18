0be5fe9332c83566031507a4e63fa0419436a336
Maciej Mejer <maciej.mejer@op.pl>
2022-09-18 20:11:35 +0200

    Initial commit

---
+++ TickTackCrypto/TickTackCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TickTackCryptoApp.swift
+//  TickTackCrypto
+//
+//  Created by Maciej Mejer on 18/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct TickTackCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
